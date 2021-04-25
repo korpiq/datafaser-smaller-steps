7249e08e57e68e3f8d2a36206af5fb70753c03b6
Kalle Hallivuori <korpiq@iki.fi>
2021-04-25 15:39:11 +0300

    Added emptiness.

--- js/__tests__/data-sources-tests/memory-source-test.js
+++ js/__tests__/data-sources-tests/memory-source-test.js
@@ -5,16 +5,26 @@ tester.describe('data-sources/memory-source', () => {
     tester.it('returns a function', () => {
         tester.expect(DataSource.Source([])).toBeInstanceOf(Function)
     })
-    
-    tester.it('reports start and end for an empty list', () => {
-        const dataSource = DataSource.Source([])
-        tester.expect(dataSource()).toBe(DataSource.LIST_BEGIN)
-        tester.expect(dataSource()).toBe(DataSource.LIST_END)
+
+    tester.it('reports empty for an empty value', () => {
+        const getNextToken = DataSource.Source()
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
     })
 
-    tester.it('reports start and end for an empty object', () => {
-        const dataSource = DataSource.Source({})
-        tester.expect(dataSource()).toBe(DataSource.DICTIONARY_BEGIN)
-        tester.expect(dataSource()).toBe(DataSource.DICTIONARY_END)
+    tester.it('reports start, end, then empty for an empty list', () => {
+        const getNextToken = DataSource.Source([])
+        tester.expect(getNextToken()).toBe(DataSource.LIST_BEGIN)
+        tester.expect(getNextToken()).toBe(DataSource.LIST_END)
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
+    })
+
+    tester.it('reports start, end, then empty for an empty object', () => {
+        const getNextToken = DataSource.Source({})
+        tester.expect(getNextToken()).toBe(DataSource.DICTIONARY_BEGIN)
+        tester.expect(getNextToken()).toBe(DataSource.DICTIONARY_END)
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
+        tester.expect(getNextToken()).toBe(DataSource.EMPTY)
     })
 })
--- js/src/data-sources/memory-source.js
+++ js/src/data-sources/memory-source.js
@@ -2,24 +2,44 @@ const LIST_BEGIN = { 'data source': 'begin list' }
 const LIST_END = { 'data source': 'end list' }
 const DICTIONARY_BEGIN = { 'data source': 'begin dictionary' }
 const DICTIONARY_END = { 'data source': 'end dictionary' }
+const EMPTY = { 'data source': 'empty' }
+const LIST_MARKERS = {
+    begin: LIST_BEGIN,
+    end: LIST_END
+}
+const DICTIONARY_MARKERS = {
+    begin: DICTIONARY_BEGIN,
+    end: DICTIONARY_END
+}
+
+const CollectionSource = (data) => {
+    let keys = undefined
+    const markers = Array.isArray(data) ?
+        LIST_MARKERS
+        : DICTIONARY_MARKERS
+    return () => {
+        if (keys === undefined) {
+            keys = Object.keys(data)
+            return markers.begin
+        }
+        if (keys) {
+            keys = false
+            return markers.end
+        }
+        return EMPTY
+    }
+}
+
+const ScalarSource = (data) => {
+    const returnable = data == undefined ? [] : [data]
+    return () => 
+        returnable.length ? returnable.shift() : EMPTY
+}
 
 const Source = (data) => {
-    let keys = undefined
-    return Array.isArray(data) ?
-        () => {
-            if (keys === undefined) {
-                keys = Object.keys(data)
-                return LIST_BEGIN
-            }
-            return LIST_END
-        }
-        : () => {
-            if (keys === undefined) {
-                keys = Object.keys(data)
-                return DICTIONARY_BEGIN
-            }
-            return DICTIONARY_END
-        }
+    return typeof data === 'object' ?
+        CollectionSource(data)
+        : ScalarSource(data)
 }
 
 module.exports = {
@@ -27,5 +47,6 @@ module.exports = {
     LIST_BEGIN,
     LIST_END,
     DICTIONARY_BEGIN,
-    DICTIONARY_END
+    DICTIONARY_END,
+    EMPTY
 }
